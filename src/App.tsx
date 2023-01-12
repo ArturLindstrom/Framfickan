64c923d786579056f636c8733aabb7e771f1991e
arturlindstrom <artur.lindstrom@gmail.com>
2023-01-12 16:37:18 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+function App() {
+  return (
+    <div className="App bg-black h-screen grid place-items-center">
+      <h1 className="neonText text-6xl text-pink-400 lg:text-9xl">
+        Framfickan
+      </h1>
+      <a href="" className="neonText underline text-5xl">
+        Meny
+      </a>
+    </div>
+  );
+}
+
+export default App;
