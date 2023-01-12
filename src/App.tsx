070051fb2a5449e144a1d5a89ada1918ef9d3318
arturlindstrom <artur.lindstrom@gmail.com>
2023-01-12 16:45:02 +0100

    asdf

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div className="App bg-black h-screen grid place-items-center">
-      <h1 className="neonText text-6xl text-pink-400 lg:text-9xl">
+      <h1 className="neonText text-7xl text-pink-400 lg:text-9xl ">
         Framfickan
       </h1>
       <a href="" className="neonText underline text-5xl">
